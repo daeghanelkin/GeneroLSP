90118b09b3b5627c4368be375b955db86ba74fcd
Elkin <daeghan.elkin@tylertech.com>
2019-09-24 16:42:34 -0400

    Function Highlighting

--- server/src/generoDeclaration.ts
+++
@@ -1,9 +0,0 @@
-import vscode = require('vscode');
-
-class GoDefinitionProvider implements vscode.DefinitionProvider {
-    public provideDefinition(
-        document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken):
-        Thenable<vscode.Location> {
-            return definitionLocation(document, position)
-    }
-}
